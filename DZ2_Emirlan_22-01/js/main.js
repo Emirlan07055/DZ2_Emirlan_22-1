78cf5848fd2b18e479fd461346120abe1d1b319e
Emirlan07055 <122383421+Emirlan07055@users.noreply.github.com>
2023-02-18 17:23:13 +0600

    DZ2_22-01

---
+++ DZ2_Emirlan_22-01/js/main.js
@@ -0,0 +1,48 @@
+let i = 0
+
+const button = document.querySelector('button')
+
+button.addEventListener("click", () => {
+    clearInterval(i)
+})
+
+button.addEventListener("click", () => {
+    const interval = setInterval(() => {
+        if (i <= 60) {
+            console.log(i++)
+        }else{
+           clearInterval(interval)
+            alert('YOUR MEMORY IS FULL')
+        }
+    }, 150)
+})
+
+/**************************/
+
+const box = document.querySelector('.box')
+
+let positionX = 0
+let positionY = 0
+
+
+const move = () => {
+    if (positionX <= 450 && positionY <= 0){
+        positionX ++
+        box.style.left = `${positionX}px`
+        setTimeout(move, 1)
+    }else if(positionX >= 440 && positionY <= 150){
+        positionY ++
+        box.style.top = `${positionY}px`
+        setTimeout(move, 1)
+    }else if(positionX >= 0 && positionY >= 150){
+        positionX --
+        box.style.left = `${positionX}px`
+        setTimeout(move, 1)
+    }else if(positionX <= 0 && positionY >= 0) {
+        positionY --
+        box.style.top = `${positionY}px`
+        setTimeout(move, 1)
+    }
+}
+move()
+
